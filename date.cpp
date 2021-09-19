b26b891818bb139725826935c415f56f604b314d
jen1ne <90233116+jen1ne@users.noreply.github.com>
2021-09-19 08:18:07 +0800

    Update date.cpp

--- date.cpp
+++ date.cpp
@@ -4,7 +4,8 @@
 char* date(){
    time_t t;
    time(&t); //time since january 1, 1970
-
+   
+   //Function to track time of each entry
    char *current_time = ctime(&t);
 
    printf("%s",current_time);
